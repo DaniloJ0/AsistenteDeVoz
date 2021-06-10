8fd2d15f65c050fcdc2a263b98b58c5af63440ff
Danilo Jimenez Rangel <rangeljd@uninorte.edu.co>
2021-06-09 19:44:54 -0500

    Final_Sin_Audio
    La libreria pyttsx3 no me funciona por lo tanto el audio
    Puede  abrir Youtube llamando por el nombre Pablo y diciendo: reproduce____________

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # AsistenteDeVoz
+
+Si Marca un error al instalar PyAudio se tiene que descargar e instalar un archivo .whl segun corresponda la versión del python
+link: https://www.lfd.uci.edu/~gohlke/pythonlibs/#pyaudio
+
+Ej: pip install PyAudio‑0.2.11‑cp39‑cp39‑win_amd64.whl
+
+2) luego de instalarlo vuelves a instalar normalmente PyAudio
+
+Ej: pip install PyAudio
--- main.py
+++ main.py
@@ -1,2 +1,36 @@
-print("goo")
-print("2ee")
+import speech_recognition as sr
+import pyttsx3 as voz
+import pywhatkit as wp
+name= "pablo"
+listener = sr.Recognizer()
+
+# engine = voz.init()
+
+# voices=engine.getProperty('voices')
+# engine.setProperty('voice',voices[0].id)
+
+# def talk(text):
+#     engine.say(text)
+#     engine.runAndWait()
+def listen():
+    try:
+        i=0
+        with sr.Microphone() as source:
+            print("Escuchando...")
+            voice=listener.listen(source)
+            rec =listener.recognize_google(voice,language="es-CO")
+            rec=rec.lower()
+            if name in rec:
+                rec =rec.replace(name, "")
+            #    talk()
+    except:
+        pass
+    return rec
+
+def run():
+    rec=listen()
+    if 'reproduce' in rec:
+        music= rec.replace("reproduce", "")
+        print("Reproduciendo "+ music)
+        wp.playonyt(music)
+run()
